5526f59c9038dc9fcc68ccf0743a0c1a5ec3142b
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2025-04-10 16:04:37 +0900

    wip

--- main.js
+++ main.js
@@ -1,40 +1,31 @@
-import { $, h as html, on, css } from "https://libh.dev";
+import { $, h as html, on, css } from "https://esm.sh/libh@0.0.90";
 
 const Main = () => {
+
 	const
-		pass = $(""),
+		length = $(16),
 
-		refreshPass = () => {
+		use_a_z = $(true),
+		use_A_Z = $(true),
+		use_1_9 = $(true),
+		use_special = $(false),
+		special = $("!#$%&'()=~|\`{+*}<>?_"),
 
-			const
-				base =
-					(use_a_z.$ ? "abcdefghijklmnopqrstuvwxyz" : "") +
-					(use_A_Z.$ ? "ABCDEFGHIJKLMNOPQRSTUVWXYZ" : "") +
-					(use_1_9.$ ? "0123456789" : "") +
-					(use_special.$ ? special.$ : "")
-				,
-				baseLength = base.length
-			;
-	
-			pass.$ = Array.from(crypto.getRandomValues(passBaseBuffer).slice(0, length.$)).map(q => base[Math.floor(q * baseLength / (2 ** 32 - 1))]).join("");
-		},
+		passBaseBuffer = new Uint32Array(1024),
 
-		length = $(16).watch(refreshPass),
-
-		use_a_z = $(true).watch(refreshPass),
-		use_A_Z = $(true).watch(refreshPass),
-		use_1_9 = $(true).watch(refreshPass),
-		use_special = $(false).watch(refreshPass),
-		special = $("!#$%&'()=~|\`{+*}<>?_").watch(refreshPass),
-
-		passBaseBuffer = new Uint32Array(1024);
+		template = length.tick()
+			.and(use_a_z.and("abcdefghijklmnopqrstuvwxyz").or(""))
+			.sum(use_A_Z.and("ABCDEFGHIJKLMNOPQRSTUVWXYZ").or(""))
+			.sum(use_1_9.and("0123456789").or(""))
+			.sum(use_special.and(special).or(""))
+			.into($ => Array.from(crypto.getRandomValues(passBaseBuffer)).slice(0, length.$).map(q => $[Math.floor(q * $.length / (2 ** 32 - 1))]).join(""))
 	;
 
-	refreshPass();
+	// refreshPass();
 
 	return html`
 
-		<label>Length: ${length}<input ${{ type: "range", value: length, min: 4, max: 1024, [css.width]: "50%" }}></label>
+		<label>Length: ${length}<input ${{ type: "range", value: length, min: 4, max: 1024, [css]: { width: "50%" } }}></label>
 		<br>
 
 		<div>
@@ -42,7 +33,7 @@ const Main = () => {
 			<label>A ~ Z<input ${{ type: "checkbox", checked: use_A_Z }}/></label>
 			<label>0 ~ 9<input ${{ type: "checkbox", checked: use_1_9 }}/></label><br>
 			<label>Special token: <input ${{ type: "checkbox", checked: use_special }}/></label>
-			<input ${{ type: "text", value: special, disabled: use_special.into($ => !$) }}/>
+			<input ${{ type: "text", value: special, disabled: use_special.not() }}/>
 		</div>
 
 		<!-- <br> -->
@@ -50,12 +41,12 @@ const Main = () => {
 		<input ${{
 			type: "button",
 			value: "copy",
-			[on.click]: () => navigator.clipboard.writeText(pass.$)
+			[on.click]: () => navigator.clipboard.writeText(template.$)
 		}}/>
 		<input ${{
 			type: "button",
 			value: "refresh",
-			[on.click]: refreshPass
+			// [on.click]: refreshPass
 		}}/>
 
 		<h1 ${{
@@ -63,7 +54,7 @@ const Main = () => {
 				filter: "blur(4px)",
 				wordBreak: "break-all",
 			}
-		}}>${pass}</h1>
+		}}>${template}</h1>
 
 
 	`;
